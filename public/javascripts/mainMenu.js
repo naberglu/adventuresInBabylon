c4660309e31461c6f83fcd85e24ca8fc366561af
nathanjohnson320 <nathanjohnson320@gmail.com>
2013-11-09 03:00:02 +0000

    Removed debug log

--- public/javascripts/mainMenu.js
+++ public/javascripts/mainMenu.js
@@ -1,13 +1,12 @@
 function mainMenu(engine) {
     //Creation of the scene 
-    var scene = new BABYLON.Scene(engine);
+    scene = new BABYLON.Scene(engine);
     scene.clearColor = [0,0,0];
     var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", Math.PI / 2, Math.PI / 3, 410, new BABYLON.Vector3(0, 0, 0), scene);
 
     // Add click event to handle clicking options
     $(window).on('click', function(event) {
 	var pickResult = scene.pick(event.clientX, event.clientY);
-	console.log(pickResult);
 	loadScene(engine, 1);
     });
 
